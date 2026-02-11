a4a7073c35978b12881ce8c76c6a33483c1182c9
Clay <claystonebarongo@gmail.com>
2026-02-11 10:35:26 +0300

    Initial commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import Sidebar from '../../components/Sidebar/Sidebar'
+
+
+const Home = () => {
+  return (
+    <>
+    <Sidebar/>
+    </>
+  )
+}
+
+export default Home
