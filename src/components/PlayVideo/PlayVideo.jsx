0832b2facb3966eca827f3978ac070a406ba584c
Clay <claystonebarongo@gmail.com>
2026-02-18 22:36:35 +0300

    daily updates

--- src/components/PlayVideo/PlayVideo.jsx
+++ src/components/PlayVideo/PlayVideo.jsx
@@ -105,7 +105,6 @@ const PlayVideo = () => {
                         <div className="comment-action">
                             <img src={like} alt="" />
                             <span>244</span>
-
                             <img src={dislike} alt="" />
                         </div>
                     </div>
