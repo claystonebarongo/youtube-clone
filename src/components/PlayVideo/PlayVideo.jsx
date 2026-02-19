b505804cd33b82a19cfa6326f86d71b9ce7409e7
Clay <claystonebarongo@gmail.com>
2026-02-19 22:48:44 +0300

    daily updates

--- src/components/PlayVideo/PlayVideo.jsx
+++ src/components/PlayVideo/PlayVideo.jsx
@@ -102,7 +102,6 @@ const PlayVideo = () => {
                     <img src={user_profile} alt="" />
                     <div>
                         <h3>Judith Muli<span>1 day ago</span></h3>
-
                         <p>Dread It or love it ,Destiny arrives all the same</p>
                         <div className="comment-action">
                             <img src={like} alt="" />
