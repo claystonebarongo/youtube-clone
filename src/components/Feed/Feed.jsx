d116077d4a6c18234952aa681d8c9175dee223f4
Clay <claystonebarongo@gmail.com>
2026-02-22 22:12:14 +0300

    daily updates

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import './Feed.css'
 import { Link } from "react-router-dom";
-
-
 import thumbnail1 from '../../assets/thumbnail1.png'
 import thumbnail2 from '../../assets/thumbnail2.png'
 import thumbnail3 from '../../assets/thumbnail3.png'
