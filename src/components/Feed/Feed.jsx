9a751253f1897cbffe471b406eb2e69c35882c73
Clay <claystonebarongo@gmail.com>
2026-02-22 22:08:57 +0300

    daily updates

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -10,8 +10,16 @@ import thumbnail5 from '../../assets/thumbnail5.png'
 import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
+import { API_KEY } from '../../data';
 
-const Feed = () => {
+const Feed = ({ category }) => {
+
+
+    const fetchData = async () => {
+        const videoList_url = ` https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
+        await fetch(videoList_url)
+    }
+
     return (
         <div className="feed">
 
